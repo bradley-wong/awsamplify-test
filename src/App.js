cf8e41b6cc19374954e6ee1c191a1288f3e06b8d
Bradley Wong <bradleyplus@gmail.com>
2019-08-22 14:38:30 -0700

    different link to twitch

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <iframe src="https://player.twitch.tv/?channel=sleightlymusical" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/sleightlymusical?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from sleightlymusical on www.twitch.tv</a>
+        <a href="https://www.twitch.tv/sleightlymusical">twitch.tv/sleightlymusical</a>
         <a
           className="App-link"
           href="https://reactjs.org"
